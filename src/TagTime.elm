ff5466481ad0d6095a79cd794dac5bf3a946ae59
Spencer Pearson <speeze.pearson+git@gmail.com>
2020-06-29 09:38:21 -0700

    refactor TagTime

--- src/TagTime.elm
+++ src/TagTime.elm
@@ -1,6 +1,9 @@
 {--
 Implementation of the TagTime Universal Ping Schedule
 Spec: https://forum.beeminder.com/t/official-reference-implementation-of-the-tagtime-universal-ping-schedule/4282
+
+This module is written in a less-than-maximally Elm-like style in order to stay easily comparable to the reference implementation.
+
 --}
 
 module TagTime exposing
@@ -20,9 +23,9 @@ import Time
 
 urPinger : Pinger
 urPinger = Pinger
-  { meanGap = 45*60         -- Average gap between pings, in seconds
-  , lastPingUnixTime = 1184097393 -- Ur-ping ie the birth of Timepie/TagTime! (unixtime)
-  , seed   = 11193462   -- Initial state of the random number generator
+  { meanGap          = 45*60        -- Average gap between pings, in seconds
+  , lastPingUnixTime = 1184097393   -- Ur-ping ie the birth of Timepie/TagTime! (unixtime)
+  , lcg              = Lcg 11193462 -- Initial state of the random number generator
   }
 
 -- Above URPING is in 2007 and it's fine to jump to any later URPING/SEED pair
@@ -32,12 +35,44 @@ urPinger = Pinger
 ia = 16807          -- =7^5: Multiplier for LCG random number generator
 im = 2147483647     -- =2^31-1: Modulus used for the RNG
 
+type Lcg = Lcg Int
+stepLcg : Lcg -> (Int, Lcg)
+stepLcg (Lcg seed) =
+  let
+    newSeed = (ia * seed) |> modBy im
+  in
+    (newSeed, Lcg newSeed)
+
+expRand : Float -> Lcg -> (Float, Lcg)
+expRand scale lcg =
+  let
+    (uniformRand, newLcg) = stepLcg lcg
+    result = -scale * logBase e (toFloat uniformRand / toFloat im)
+  in
+    (result, newLcg)
 
 
-fromTime : Time.Posix -> Pinger
-fromTime time =
-  stepUntil time urPinger
+type Pinger = Pinger
+  { meanGap : Int
+  , lastPingUnixTime : Int
+  , lcg : Lcg
+  }
 
+
+nextPing : Pinger -> (Time.Posix, Pinger)
+nextPing pinger =
+  let
+    (Pinger internals) = pinger
+    (eRand, newLcg) = expRand (meanGap pinger |> toFloat) internals.lcg
+    gap = max 1 (round eRand)
+    resultUnixTime = internals.lastPingUnixTime + gap
+  in
+    ( Time.millisToPosix (resultUnixTime*1000)
+    , Pinger { internals | lcg = newLcg , lastPingUnixTime = resultUnixTime }
+    )
+
+
+-- equivalent of init() in the reference implementation
 stepUntil : Time.Posix -> Pinger -> Pinger
 stepUntil time pinger =
   let
@@ -49,45 +84,27 @@ stepUntil time pinger =
       stepUntil time steppedPinger
 
 
-type Pinger = Pinger
-  { meanGap : Int
-  , lastPingUnixTime : Int
-  , seed : Int
-  }
+
+-- EXTRA FUNCTIONS FOR MANIPULATING PINGERS
+
+lastPing : Pinger -> Time.Posix
+lastPing (Pinger {lastPingUnixTime}) =
+  Time.millisToPosix (lastPingUnixTime*1000)
 
 meanGap : Pinger -> Int
 meanGap (Pinger internals) =
   internals.meanGap
 
+fromTime : Time.Posix -> Pinger
+fromTime time =
+  stepUntil time urPinger
 
 
-stepLcg : Pinger -> (Int, Pinger)
-stepLcg (Pinger internals) =
-  let
-    {seed} = internals
-    newSeed = (ia * seed) |> modBy im
-  in
-    (newSeed, Pinger {internals | seed  = newSeed})
 
-nextGap : Pinger -> (Int, Pinger)
-nextGap pinger =
-  let
-    (uniformRand, newPinger) = stepLcg pinger
-    expRand = -(meanGap pinger |> toFloat) * logBase e (toFloat uniformRand / toFloat im)
-    gap = max 1 (round expRand)
-  in
-    (gap, newPinger)
 
-nextPing : Pinger -> (Time.Posix, Pinger)
-nextPing pinger =
-  let
-    (gap, Pinger internalsWithNewLcg) = nextGap pinger
-    resultUnixTime = internalsWithNewLcg.lastPingUnixTime + gap
-  in
-    ( Time.millisToPosix (resultUnixTime*1000)
-    , Pinger { internalsWithNewLcg | lastPingUnixTime = resultUnixTime }
-    )
 
+
+-- DEBUGGING STUFF THAT I SHOULD PACKAGE INTO TESTS
 
 firstFewPings : List (Time.Posix)
 firstFewPings =
@@ -118,11 +135,24 @@ Tuple.first (nextPing <| stepUntil <| Time.millisToPosix 1184104776000) == Time.
 
 --}
 
-
+type alias Model = { pings : List Time.Posix, pinger : Pinger }
 main = Browser.sandbox { init=init, view=view, update=update }
-init : Pinger
-init = urPinger
-view : Pinger -> Html.Html ()
-view pinger = Html.div [] [Html.button [Html.Events.onClick ()] [Html.text "step"], Html.text (Debug.toString pinger)]
-update : () -> Pinger -> Pinger
-update _ pinger = Tuple.second (nextPing pinger)
+init : Model
+init = {pings=[], pinger=urPinger}
+view : Model -> Html.Html ()
+view {pings, pinger} = Html.div []
+  [ Html.button [Html.Events.onClick ()] [Html.text "step"]
+  , Html.text (Debug.toString pinger)
+  , Html.br [] []
+  , Html.text "Past pings:"
+  , pings
+    |> List.map (Time.posixToMillis >> (\x -> x//1000) >> String.fromInt >> Html.text >> List.singleton >> Html.li [])
+    |> Html.ul []
+  ]
+update : () -> Model -> Model
+update () model =
+  let (p, pr) = nextPing model.pinger in
+  { model
+  | pings = p :: model.pings
+  , pinger = pr
+  }
